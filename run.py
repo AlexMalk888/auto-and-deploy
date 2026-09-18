308cef78b53f7a9e8bd36a6a9ce40b6ffc0046aa
AlexMalk888 <malkov.a888@mail.ru>
2026-09-18 18:24:12 +0300

    V0.0

---
+++ run.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import configparser
+
+import yfinance as yf
+from datetime import datetime, timedelta
+from pgdb import PGDatabase
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+COMPANIES = eval(config['Companies']['COMPANIES'])
+SALES_PATH = config['Files']['SALES_PATH']
+DATABASE_CREDS = config['Database']
+
+sales_df = pd.DataFrame()
+if os.path.exists(SALES_PATH):
+    sales_df = pd.read_csv(SALES_PATH)
+    # print(sales_df)
+    os.remove(SALES_PATH)
+
+
+
+historical_d = {}
+
+for company in COMPANIES:
+    historical_d[company] = yf.download(company, start=(datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d'), end=datetime.today().strftime('%Y-%m-%d'))
+# print(historical_d)
+
+database = PGDatabase(
+    host=DATABASE_CREDS['HOST'],
+    database=DATABASE_CREDS['DATABASE'],
+    user=DATABASE_CREDS['USER'],
+    password=DATABASE_CREDS['PASSWORD']
+)
+
+for i, row in sales_df.iterrows():
+    query = f"insert into sales values ('{row['dt']}', '{row['company']}', '{row['transaction_type']}', {row['amount']})"
+    print(query)
+    database.post(query)
+
+for company, data in historical_d.items():
+    for date, row in data.iterrows():
+        query = f"insert into stock values ('{date.strftime('%Y-%m-%d')}', '{company}', {round(row[('Open', company)], 2)}, {round(row[('Close', company)], 2)})"
+        database.post(query)
