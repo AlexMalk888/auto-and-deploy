972f93d0df6de0482299e94e7b2018559f202907
AlexMalk888 <malkov.a888@mail.ru>
2026-09-22 17:38:32 +0300

    Fix script

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ import yfinance as yf
 from datetime import datetime, timedelta
 from pgdb import PGDatabase
 
+dirname = os.path.dirname(__file__)
+
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(os.path.join(dirname, 'config.ini'))
 
 COMPANIES = eval(config['Companies']['COMPANIES'])
 SALES_PATH = config['Files']['SALES_PATH']
@@ -35,7 +37,6 @@ database = PGDatabase(
 
 for i, row in sales_df.iterrows():
     query = f"insert into sales values ('{row['dt']}', '{row['company']}', '{row['transaction_type']}', {row['amount']})"
-    print(query)
     database.post(query)
 
 for company, data in historical_d.items():
